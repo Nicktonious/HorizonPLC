186a0eca45d78746a103cec826419f5333b8bd86
Nictonious <medovoy1213@gmail.com>
2024-12-27 09:36:42 +0400

    feat: реализовано создание tcp-сервера feat: переработан перехват консоли: поток с REPL связывается с открывающимся сокетом feat: добавлены обработчики закрытия соединения (намеренного и в случае ошибки); в таком случае попытка поднять новый сервер или возвращение стандартной консоли feat: добавлено чтение параметров _opts.port и _opts.autoRouteOn в конструкторе feat: добавлен геттер ConsoleType, возвращающий тип консоли: USB/NONE/REMOTE; требует доработки при переходе на espr2v25, т.к на данный момент отсутствует возможность узнать что PLC никак не подключен

--- js/plcRouteREPL/js/plcRouteREPL.js
+++ js/plcRouteREPL/js/plcRouteREPL.js
@@ -1,143 +1,87 @@
 /**
  * @class
- * Класс реализует перехват ввода и вывода из консоли, включая в этот процесс внешние источники. 
- * Прием сообщений происходит по возникновению события 'repl-write'.
- * Отзеркаливание и передача данных из REPL происходит по событию ''repl-read.  
+ * Класс предоставляет возможность удаленного подключения к консоли по TCP-соединению.
  */
 class ClassRouteREPL {
-    constructor() {                         
-        this._InBuffer = '';
-        this._DefConsole = eval(E.getConsole()); // eval позволяет хранить инстанцированный объект UART шины. Это необходимо для работы с его функционалом из класса Route 
-        this._IncrVal = 0;
-        this._MasterID = 'EWI';     
+    constructor(_opts) {
+        _opts = _opts || {};                         
+        this._DefConsole = eval(E.getConsole()); // eval позволяет хранить инициализированный объект UART шины. Это необходимо для работы с его функционалом из класса Route   
         this._IsOn = false;     
         this._Name = 'RouteREPL';
-
-        Object.on('repl-sub', () => {
-            if (!this._IsOn) this.RouteOn();
-        });
+        this._ReconnectTry = 0;
+        this._Port = _opts.port || 23;
+        // авто запуск роутинга после полного старта PLC
+        if (_opts.autoRouteOn) 
+            Object.on('complete', this.RouteOn.bind(this));
     }
-    /**
-     * Команда, по которой RouteREPL переназначает Master-устройство
-     * @returns {Number}
+    /** 
+     * @getter 
+     * Возвращает тип текущего подключения к консоли: NONE, USB или REMOTE
      */
-    get NEW_MASTER_COMMAND() { return '@@C_M@@'; }
-    /**
-     * Автоикрементирующийся индекс для входящих и исходящих сообщений
-     * @returns {Number}
-     */
-    get IncrID() { return ++this._IncrVal; }
-    /**
-     * @method
-     * Метод включает обработку событий "repl-cm", "repl-write",
-     * которые осуществляют обмен данными между RoutREPL и внешней средой,
-     * "repl-cm" который устанавливает новое значение мастера
-     */
-    InitEvents() {       
-        Object.on('repl-write', (commands, id) => {
-            if (id === this._MasterID) {
-                commands.forEach(command => {
-                    this.Receive(`${command}\r`);
-                });
-            }
-        });
-
-        Object.on('repl-cm', id => this.ChangeMaster(id));
+    get ConsoleType() {
+        let cons = E.getConsole();
+        return (!cons) ? 'NONE' :
+                cons.startsWith('Loopback') ? 'REMOTE' : 'USB';
     }
     /**
      * @method
-     * Обработчик события, вызываемого по поступлению данных из REPL на LoopbackB 
-     * @param {String} data 
-     */
-    LoopbackBHandler(data) {
-        this._DefConsole.write(' ' + data);
-        Object.emit('repl-read', data);
-    }
-    /**
-     * @method
-     * Обработчик события, вызываемого по поступлению данных со стандартной консоли
-     */
-    DefConsoleHandler(data) {
-        this._DefConsole.write(data); 
-        this._InBuffer += data;             //заполнение буффера введеными символами
-        if (data === '\r') {
-            let command = this._InBuffer;
-            this._InBuffer = '';
-
-            if (this._MasterID === 'EWI') this.Receive(command);  //проверка на то что была введена команда смены мастера
-            
-            else if (command.indexOf(this.NEW_MASTER_COMMAND) !== -1) {
-                this.ChangeMaster('EWI');
-            }
-        }
-    }
-    /**
-     * @method
-     * Перехват консоли, настройка ивентов для обмена сообщениями между REPL, EWI и внешними устройствами
+     * Запуск TCP-сервера
+     * Перехват консоли при подключении клиента. Объединение потоков с консоли на сокет и обратно.
      */
     RouteOn() {
-        E.setConsole(LoopbackA, { force: true });   //Перехватываем консоль
-
-        this.InitEvents();
-
-        LoopbackB.on('data', data => {              //настраиваем обработку данных, поступающих с REPL
-            this.LoopbackBHandler(data);
-        });
-
-        this._DefConsole.on('data', data => {       //настраиваем обработку данных, поступающих с консоли
-            this.DefConsoleHandler(data);
-        });
+        try {
+            this._Server = require('net').createServer(_socket => {
+                // завершение предыдущего подключения
+                if (this._Socket) this._Socket.end();
+                this._Socket = _socket;
+                _socket.on('close', () => {
+                    this._Socket = null;
+                    setTimeout(() => {
+                        // возврат стандартной консоли если не появился новый сокет
+                        if (!this._Socket) this.RouteOff();
+                    }, 50);
+                })
+                // перехват и перенаправление консоли на сокет
+                _socket.pipe(LoopbackB);
+                LoopbackB.pipe(_socket);
+                E.setConsole(LoopbackA, { force: true });   //Перехватываем консоль
+            });
+            this._Server.listen(this._Port);
+        } catch (e) {
+            H.Logger.Service.Log({ service: 'RouteREPL', level: 'I', msg: e });
+            if (++this._ReconnectTry < 3) {
+                this.RouteOn();
+            } else {
+                this.RouteOff();
+                this._ReconnectTry = 0;
+            }
+        }
 
         this._IsOn = true;
     }
     /**
      * @method
      * Через этот метод RouteREPL получает команду к непосредственно выполнению.
-     * @param {String} command - команда, которая передается в REPL
+     * @param {String} _stdin - команда, которая передается в REPL
      * @returns 
      */
-    Receive(command) {
-        if (!this._IsOn) return false; 
-        Object.emit('repl-read', command);  //"отзеркаливание" входного сообщения
-        // TODO: продумать необходмимо ли дополнительно обрамлять отзеркаливаемое сообщение
-        LoopbackB.write(command);
-        return true;
+    Receive(_stdin) {
+        LoopbackB.write(_stdin);
     }
-    /**
-     * @method
-     * Метод, который меняет текущего мастера
-     * @param {String} id - идентификатор нового мастера
-     */
-    ChangeMaster(id) {
-        this._MasterID = id;
-        this._DefConsole.write('repl-read', this.ToMsgPattern(`Info>> New MasterREPL, ID: ${this._MasterID}`));  //TODO: проверить насколько этот формат отправки сообщения соответствует общей методолгии
+
+    isREPLConnected(_flag) {
+        return _flag;
     }
+    
     /**
      * @method 
      * Возвращает работу консоли в состояние по умолчанию (как при запуске Espruino IDE). 
      * Рассчитан на применение сугубо в целях отладки.
      */
-    SetOff() {
+    RouteOff() {
         E.setConsole(this._DefConsole, { force: true });
+        if (this._Socket) this._Socket.end();
         this._IsOn = false;
-    }
-    /**
-     * @method
-     * Формирует выходное сообщение
-     * @param {String} string - Текст сообщения
-     * @param {String} [id] - ID отправителя
-     * @returns {String}
-     */
-    ToMsgPattern(str, id) {
-        // TODO: код ниже задокументирован до принятия решения касательно форматирования исходящих сообщений
-        // if (id) return `${this.IncrID} <${id}> ${str}}`;
-
-        // return `${this.IncrID} ${str}`;
-
-        return str;
-    }
-    isREPLConnected(_flag) {
-        return _flag;
     }
 }
 exports = ClassRouteREPL;
