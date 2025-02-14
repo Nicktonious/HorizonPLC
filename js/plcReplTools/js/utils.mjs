0708abf1b45e8bc6fa2c9d97b2ecc8cbfae8f0fc
Nictonious <medovoy1213@gmail.com>
2025-02-14 10:26:00 +0400

    feat: в utils вынесен getAppArgs feat: добавлен getSystemTime аналогичный используемому на PLC

--- js/plcReplTools/js/utils.mjs
+++ js/plcReplTools/js/utils.mjs
@@ -2,6 +2,35 @@ import fs from 'fs';
 import path from 'path';
 
 /**
+ * @function 
+ * @description Собирает аргументы командной строки
+ * @returns {object}
+ */
+const getAppArgs = () => {
+    const args = process.argv.slice(2); // Убираем первые два аргумента
+    const parsedArgs = {};
+    let currentKey = null;
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i].startsWith('-')) {
+            currentKey = args[i].replace('-', '');
+            parsedArgs[currentKey] = []; // Создаем массив для хранения значений
+        } else if (currentKey) {
+            parsedArgs[currentKey].push(args[i]); // Добавляем значение в массив
+        }
+    }
+
+    // Преобразуем массивы с одним элементом в строку (как раньше)
+    Object.keys(parsedArgs).forEach(key => {
+        if (parsedArgs[key].length === 1) {
+            parsedArgs[key] = parsedArgs[key][0];
+        }
+    });
+
+    return parsedArgs;
+};
+
+/**
  * Обходит все папки и находит непустые файлы
  * @param {string} dir Путь к директории для обхода
  * @returns {Promise<string[]>} Список путей к непустым файлам
@@ -15,16 +44,11 @@ async function findNonEmptyFiles(dir) {
     for (const entry of entries) {
         const fullPath = path.join(dir, entry.name);
 
-        /*if (entry.isDirectory()) {
-            // Если это папка, обходим её рекурсивно
-            const subDirFiles = await findNonEmptyFiles(fullPath);
-            nonEmptyFiles = nonEmptyFiles.concat(subDirFiles);
-        } else */
         if (entry.isFile()) {
             // Если это файл, проверяем его размер
             const stats = await fs.promises.stat(fullPath);
             if (stats.size > 0) {
-                nonEmptyFiles.push(fullPath);
+                nonEmptyFiles.push(entry.name);
             }
         }
     }
@@ -32,5 +56,11 @@ async function findNonEmptyFiles(dir) {
     return nonEmptyFiles;
 }
 
+const getSystemTime = () => {
+    let date = new Date();
+    return (date.getFullYear() + "-" + ("0" + (date.getMonth() + 1)).substr(-2) +
+        "-" + ("0" + date.getDate()).substr(-2) + " " + ("0" + date.getHours()).substr(-2) +
+        ":" + ("0" + date.getMinutes()).substr(-2) + ":" + ("0" + date.getSeconds()).substr(-2));
+}
 
-export { findNonEmptyFiles };
+export { getAppArgs, findNonEmptyFiles, getSystemTime };
